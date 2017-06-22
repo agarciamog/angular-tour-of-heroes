1e62046791c234714f0b5da0c29e146221272240
Alberto Garcia Gonzalez <agarciamog@gmail.com>
2017-06-22 16:04:10 -0600

    Added InMemoryWebApiModule and Refactored HeroService
    Installed node package angular-in-memory-web-api, basically intercepts
    Angular Http request that would otherwise go to a remote server. Had to
    change hero.service.ts methods getHero and getHeroes to use in memory
    api.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent} from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
 import { AppRoutingModule } from './app-routing.module';
+
+// For in-memory web service
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service'
 
 @NgModule({
   declarations: [
@@ -19,7 +25,9 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    HttpModule,
+    AppRoutingModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
